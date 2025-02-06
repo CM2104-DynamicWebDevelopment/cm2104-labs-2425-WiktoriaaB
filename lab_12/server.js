5a06dd6575fa785519c27af8bdd2bcc31a93585a
WiktoriaaB <w.bardzik@rgu.ac.uk>
2025-02-06 12:02:42 +0000

    lab12_8

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,66 +1,56 @@
-var express = require('express');
+var express =  require('express');
 var app = express();
 var SpotifyWebApi = require('spotify-web-api-node');
-app.use(express.static('public'))
-
-
+app.use(express.static('public'));
 app.get('/', function(req, res){
-res.send("Hello world! by express");
+    res.send("Hello world! by express");
 });
-
 var spotifyApi = new SpotifyWebApi({
-    clientId: 'a07ae88c2087471aa3b9d4d1198edcf2',
-    clientSecret: 'dbdd7a396e6747bebb452b3d7a82961d'
+    clientId: '307619f0099d47608888f7f73bd463b8',
+    clientSecret: '6a03278a9df94716bce210b8446f8d15'
 });
 
-spotifyApi.clientCredentialsGrant().then( 
+spotifyApi.clientCredentialsGrant().then(
     function (data) {
         console.log('The access token expires in ' + data.body['expires_in']);
         console.log('The access token is ' + data.body['access_token']);
-
-       
-        spotifyApi.setAccessToken(data.body['access_token']); 
-
-        },
-        function (err) {
+        spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function (err) {
         console.log(
-        'Something went wrong when retrieving an access token',
-        err.message 
+            'Something went wrong when retrieving an access token',
+            err.message
         );
     }
 );
-
-async function getTracks(searchterm, res) { 
-
+async function getTracks(searchterm, res) {
     spotifyApi.searchTracks(searchterm)
         .then(function (data) {
-            spotifyApi.searchTracks(searchterm)
-            .then(function (data) {
-            var tracks = data.body.tracks.items 
+            var tracks = data.body.tracks.items
             var HTMLResponse = "";
-         
             for(var i=0; i<tracks.length;i++){
-            
                 var track = tracks[i];
                 console.log(track.name);
-                    HTMLResponse = HTMLResponse +
+                HTMLResponse = HTMLResponse +
                     "<div>" +
                     "<h2>"+track.name+"</h2>"+
                     "<h4>"+track.artists[0].name+"</h4>"+
                     "<img src='"+track.album.images[0].url +"'>"+
-                    "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+
-                    "</div>";
-                    console.log(HTMLResponse);
-                }
-                    res.send(HTMLResponse)
-                }, function (err) {
-                    console.error(err);
-                });    
-        }); 
+                    "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+"</div>";
+                console.log(HTMLResponse);
+            }
+            res.send(HTMLResponse)
+        }, function (err) {
+            console.error(err);
+        });
 }
-
+app.get('/search', function (req, res) {
+    var searchterm = req.query.searchterm;
+    getTracks(searchterm, res);
+});
 app.get('/searchLove', function (req, res) {
     getTracks('love', res);
 });
 
 app.listen(8080);
+ 
