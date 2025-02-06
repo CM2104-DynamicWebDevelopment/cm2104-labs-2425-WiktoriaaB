12d726ccc0a712b250f6bdbbb0c78646d5e8bc9e
WiktoriaaB <w.bardzik@rgu.ac.uk>
2025-02-06 11:08:10 +0000

    lab12_6

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 var SpotifyWebApi = require('spotify-web-api-node');
- app.use(express.static('public'))
+app.use(express.static('public'))
 
 
- app.get('/', function(req, res){
- res.send("Hello world! by express");
+app.get('/', function(req, res){
+res.send("Hello world! by express");
 });
 
 var spotifyApi = new SpotifyWebApi({
@@ -34,6 +34,28 @@ async function getTracks(searchterm, res) {
 
     spotifyApi.searchTracks(searchterm)
         .then(function (data) {
+            spotifyApi.searchTracks(searchterm)
+            .then(function (data) {
+            var tracks = data.body.tracks.items 
+            var HTMLResponse = "";
+         
+            for(var i=0; i<tracks.length;i++){
+            
+                var track = tracks[i];
+                console.log(track.name);
+                    HTMLResponse = HTMLResponse +
+                    "<div>" +
+                    "<h2>"+track.name+"</h2>"+
+                    "<h4>"+track.artists[0].name+"</h4>"+
+                    "<img src='"+track.album.images[0].url +"'>"+
+                    "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+
+                    "</div>";
+                    console.log(HTMLResponse);
+                }
+                    res.send(HTMLResponse)
+                }, function (err) {
+                    console.error(err);
+                });    
         res.send(JSON.stringify(data.body));
         }, function (err) {
         console.error(err);
