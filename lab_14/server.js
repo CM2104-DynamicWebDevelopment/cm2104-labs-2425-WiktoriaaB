fcb6869c1d5d4ba3e3a2caa962a4b1dff16de785
WiktoriaaB <w.bardzik@rgu.ac.uk>
2025-02-20 10:38:32 +0000

    lab14_5

--- lab_14/server.js
+++ lab_14/server.js
@@ -7,25 +7,24 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 // index page
 //app.get('/', function(req, res) {
- //res.render('pages/index');
+//    res.render('pages/index');
 //});
-// about page
-// new route
+// index page
 app.get('/', function(req, res) {
     var drinks = [
-    { name: 'Bloody Mary', drunkness: 3 },
-    { name: 'Martini', drunkness: 5 },
-    { name: 'Scotch', drunkness: 10 }
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
     ];
-    var tagline = "Any code of your own that you havent looked at for six or more months might as well have been written by someoneelse.";
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
     res.render('pages/index', {
-    drinks: drinks,
-    tagline: tagline
+        drinks: drinks,
+        tagline: tagline
     });
-   });
-
+});
+// about page
 app.get('/about', function(req, res) {
- res.render('pages/about');
+    res.render('pages/about');
 });
 app.listen(8080);
 console.log('8080 is the magic port');
